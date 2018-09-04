08eb3c9d01f2df83acb1bc2bd202e445aa18285d
Julio Chana <julio@spotahome.com>
2018-09-04 16:06:31 +0200

    [DEVOPS-824] Set a maximum name and label length of 60 characters

--- operator/redisfailover/service/names.go
+++ operator/redisfailover/service/names.go
@@ -6,6 +6,10 @@ import (
 	redisfailoverv1alpha2 "github.com/spotahome/redis-operator/api/redisfailover/v1alpha2"
 )
 
+const (
+	maxNameLength = 60
+)
+
 // GetRedisShutdownConfigMapName returns the name for redis configmap
 func GetRedisShutdownConfigMapName(rf *redisfailoverv1alpha2.RedisFailover) string {
 	if rf.Spec.Redis.ShutdownConfigMap != "" {
@@ -30,5 +34,14 @@ func GetSentinelName(rf *redisfailoverv1alpha2.RedisFailover) string {
 }
 
 func generateName(typeName, metaName string) string {
-	return fmt.Sprintf("%s%s-%s", baseName, typeName, metaName)
+	generatedName := fmt.Sprintf("%s%s-%s", baseName, typeName, metaName)
+
+	// If lenth higher than 60 characters, truncate the name so it does not cause problem when creating resources
+	// Applicable for names and labels
+	// https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#syntax-and-character-set
+	if len(generatedName) > maxNameLength {
+		generatedName = generatedName[:maxNameLength]
+	}
+
+	return generatedName
 }
