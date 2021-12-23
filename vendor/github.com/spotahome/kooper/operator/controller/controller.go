57bc312117ecee9398d11f0594fbd2279e41f6f1
Sergio Ballesteros <snaker@locolandia.net>
2021-12-23 17:07:29 +0100

    Update packages for kooper v2.1.0

--- vendor/github.com/spotahome/kooper/operator/controller/controller.go
+++
@@ -1,9 +0,0 @@
-package controller
-
-// Controller is the object that will implement the different kinds of controllers that will be running
-// on the application.
-type Controller interface {
-	// Run runs the controller, it receives a channel that when receiving a signal it will stop the controller,
-	// Run will block until it's stopped.
-	Run(stopper <-chan struct{}) error
-}
