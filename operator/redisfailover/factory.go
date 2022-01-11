c9fe6a66fcf7e48cf1b2844f1b8dceaf6c1ed498
Sergio Ballesteros <snaker@locolandia.net>
2022-01-11 09:11:20 +0100

    Migrate to kooper v2

--- operator/redisfailover/factory.go
+++ operator/redisfailover/factory.go
@@ -1,11 +1,15 @@
 package redisfailover
 
 import (
+	"context"
 	"time"
 
-	kmetrics "github.com/spotahome/kooper/monitoring/metrics"
-	"github.com/spotahome/kooper/operator"
-	"github.com/spotahome/kooper/operator/controller"
+	"github.com/spotahome/kooper/v2/controller"
+	kooperlog "github.com/spotahome/kooper/v2/log"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/tools/cache"
 
 	"github.com/spotahome/redis-operator/log"
 	"github.com/spotahome/redis-operator/metrics"
@@ -21,23 +25,42 @@ const (
 
 // New will create an operator that is responsible of managing all the required stuff
 // to create redis failovers.
-func New(cfg Config, k8sService k8s.Services, redisClient redis.Client, mClient metrics.Instrumenter, kooperMetricsRecorder kmetrics.Recorder, logger log.Logger) operator.Operator {
-	logger = logger.With("operator", operatorName)
-
-	// Create our CRDs.
-	watchedCRD := newRedisFailoverCRD(k8sService, logger)
-
+func New(cfg Config, k8sService k8s.Services, redisClient redis.Client, kooperMetricsRecorder metrics.Recorder, logger log.Logger) (controller.Controller, error) {
 	// Create internal services.
 	rfService := rfservice.NewRedisFailoverKubeClient(k8sService, logger)
 	rfChecker := rfservice.NewRedisFailoverChecker(k8sService, redisClient, logger)
 	rfHealer := rfservice.NewRedisFailoverHealer(k8sService, redisClient, logger)
 
 	// Create the handlers.
-	rfHandler := NewRedisFailoverHandler(cfg, rfService, rfChecker, rfHealer, k8sService, mClient, logger)
+	rfHandler := NewRedisFailoverHandler(cfg, rfService, rfChecker, rfHealer, k8sService, kooperMetricsRecorder, logger)
+	rfRetriever := NewRedisFailoverRetriever(k8sService)
 
 	// Create our controller.
-	ctrl := controller.NewSequential(resync, rfHandler, watchedCRD, kooperMetricsRecorder, logger.WithField("controller", "redisfailover"))
+	return controller.New(&controller.Config{
+		Handler:         rfHandler,
+		Retriever:       rfRetriever,
+		MetricsRecorder: kooperMetricsRecorder,
+		Logger:          kooperlogger{Logger: logger.WithField("operator", "redisfailover")},
+		Name:            "redisfailover",
+		ResyncInterval:  resync,
+	})
+}
 
-	// Assemble all in an operator.
-	return operator.NewOperator(watchedCRD, ctrl, logger)
+func NewRedisFailoverRetriever(cli k8s.Services) controller.Retriever {
+	return controller.MustRetrieverFromListerWatcher(&cache.ListWatch{
+		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+			return cli.ListRedisFailovers(context.Background(), "", options)
+		},
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			return cli.WatchRedisFailovers(context.Background(), "", options)
+		},
+	})
+}
+
+type kooperlogger struct {
+	log.Logger
+}
+
+func (k kooperlogger) WithKV(kv kooperlog.KV) kooperlog.Logger {
+	return kooperlogger{Logger: k.Logger.WithFields(kv)}
 }
