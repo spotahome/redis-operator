e625fa506d148794ca5ea3884e0c45ddeeab9107
Parsa <parsa_ghorbani@outlook.com>
2022-03-02 15:11:03 +0330

    Revert changes on build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,17 @@ set -o nounset
 src=./cmd/redisoperator
 out=./bin/redis-operator
 
-final_out=${out}
+if [[ ! -z ${TARGETOS} ]] && [[ ! -z ${TARGETARCH} ]];
+then
+    echo "Building ${TARGETOS}/${TARGETARCH} release..."
+    export GOOS=${TARGETOS}
+    export GOARCH=${TARGETARCH}
+    binary_ext=-${TARGETOS}-${TARGETARCH}
+else
+    echo "Building native release..."
+fi
+
+final_out=${out}${binary_ext}
 ldf_cmp="-w -extldflags '-static'"
 f_ver="-X main.Version=${VERSION:-dev}"
 
